f36828f70bdbd666e929a531242dd3449d43a57c
Chedy soltani <chedysoltani0@gmail.com>
2025-04-15 15:18:52 +0100

    design login

--- mon-projet-frontend/src/main.ts
+++ mon-projet-frontend/src/main.ts
@@ -1,8 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes)],
+  providers: [
+    provideHttpClient(),
+    provideAnimations(),
+    provideRouter(routes)
+  ],
 });
